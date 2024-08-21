bc2bb0969f81b111dedd97a091b9df89a795b3da
Abdulkarim Mahboob <122874449+KemoMahboob@users.noreply.github.com>
2024-08-21 17:28:18 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras.utils import img_to_array, load_img
 import numpy as np
 from PIL import Image
 
@@ -17,23 +18,20 @@ if uploaded_file is not None:
     image_path = Image.open(uploaded_file)
     
     # Display the uploaded image
-    st.image(img, caption='Uploaded Image', use_column_width=True)
+    st.image(image_path, caption='Uploaded Image', use_column_width=True)
     st.write("")
     st.write("Classifying...")
     
     # Preprocess the image to match the input format of the model
-    img = img.resize((150, 150))  # Resize to the same size as used during model training
-    image = keras.utils.load_img(image_path)
-    input_arr = keras.utils.img_to_array(image)
-    input_arr = np.array([input_arr])  # Convert single image to a batch.
-    predictions = model.predict(input_arr)
-
+    img = image_path.resize((150, 150))  # Resize to the same size as used during model training
+    img_array = img_to_array(img) / 255.0  # Normalize the image
+    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+    
     # Make prediction
+    prediction = model.predict(img_array)
     
     # Output the result
     if prediction[0] > 0.5:
         st.write("It's a Dog!")
     else:
         st.write("It's a Cat!")
-
-
